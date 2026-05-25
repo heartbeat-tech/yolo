ce409fceedbfabd8cdc1102a86f49d487c963e5b
UltralyticsAssistant <web@ultralytics.com>
2026-05-25 05:23:06 +0000

    Auto-format by https://ultralytics.com/actions

--- train1.py
+++ train1.py
@@ -1,7 +1,9 @@
 import multiprocessing
 import random
+
 import numpy as np
 import torch
+
 from ultralytics import YOLO
 
 
@@ -14,7 +16,7 @@ def set_seed(seed=42):
     print(f"✅ 随机种子已固定为: {seed}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     multiprocessing.freeze_support()
     set_seed(42)
     torch.cuda.empty_cache()
@@ -41,8 +43,7 @@ if __name__ == '__main__':
         seed=42,  # 双重保险
         deterministic=True,  # 增加确定性模式
         plots=True,  # 画训练曲线
-
     )
 
-    print(f"\n✅ 训练完成！")
+    print("\n✅ 训练完成！")
     print(f"📊 mAP50: {results.results_dict['metrics/mAP50(B)']:.3f}")
